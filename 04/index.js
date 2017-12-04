0018413a799b5aec908156f2f17d37226c1723e7
John Van Horn <jmvanhorn@gmail.com>
2017-12-04 16:03:52 -0600

    Day 4, part 2

---
+++ 04/index.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync('./input.txt', 'utf8').split('\n').map(s => s.trim());
+
+const hasAnagram = phrase => {
+  const words = phrase.split(' ');
+  return words.length !== new Set(words.map(w => w.split('').sort().join(''))).size;
+};
+const isValidViaAnagram = phrase => !hasAnagram(phrase);
+
+// const assert = require('assert');
+// assert.equal(isValidViaAnagram('abcde fghij'), true);
+// assert.equal(isValidViaAnagram('abcde xyz ecdab'), false);
+// assert.equal(isValidViaAnagram('a ab abc abd abf abj'), true);
+// assert.equal(isValidViaAnagram('iiii oiii ooii oooi oooo'), true);
+// assert.equal(isValidViaAnagram('oiii ioii iioi iiio'), false);
+// console.log('✅');
+
+console.log('num valid (no anagram):', input.filter(isValidViaAnagram).length);
