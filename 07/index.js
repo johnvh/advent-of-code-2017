48eae975834785bdae5731d87b93b41e005307f4
John Van Horn <jmvanhorn@gmail.com>
2017-12-07 23:18:09 -0600

    Day 7, part 2

--- 07/index.js
+++ 07/index.js
@@ -1,4 +1,5 @@
 const P = require('parsimmon');
+const {pipe, assoc, sum} = require('ramda');
 const toInt = n => parseInt(n, 10);
 const parser = P.seqObj(
   ['name', P.letters],
@@ -11,20 +12,28 @@ const parser = P.seqObj(
   ]
 );
 // [{name:string, weight:number, children:string[]}]
-const tower = require('fs').readFileSync('./input.txt', 'utf8')
-  .trim()
-  .split('\n')
-  .map(parser.tryParse.bind(parser));
+const tower = new Map(
+  require('fs').readFileSync('./input.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(pipe(
+      parser.tryParse.bind(parser),
+      t => [t.name, t]
+    ))
+);
 
 // console.log(parser.parse('rxivjo (206) -> mewof, hrncqs, qgfstpq'));
 // console.log(parser.parse('rxivjo (206)'));
-// console.log(tower.slice(0, 2));
+// console.log([...tower.values()].slice(0, 2));
 
+const parentNode = (tower, name) => [...tower.values()]
+  .find(node => node.children.includes(name));
 const root = tower => {
-  let curr = tower[0];
+  let [curr] = tower.values();
 
   while (true) {
-    const parent = tower.find(node => node.children.includes(curr.name));
+    // const parent = [...tower.values()].find(node => node.children.includes(curr.name));
+    const parent = parentNode(tower, curr.name);
     if (!parent) {
       return curr;
     }
@@ -33,3 +42,73 @@ const root = tower => {
 };
 
 console.log('root:', root(tower).name);
+console.log('='.repeat(80));
+
+const nodeWithFullChildren = (tower, name) => {
+  const node = pipe(
+    name => tower.get(name),
+    node => assoc(
+      'children',
+      node.children.map(name => tower.get(name)),
+      node
+    )
+  )(name);
+  return node;
+};
+const nodeWeight = (tower, name) => {
+  const node = pipe(
+    name => tower.get(name),
+    node => assoc(
+      'children',
+      node.children.map(name => nodeWeight(tower, name)),
+      node
+    )
+  )(name);
+
+  return sum([
+    node.weight,
+    ...node.children
+  ]);
+};
+
+// cheating... 😎
+
+console.log(pipe(
+  () => nodeWithFullChildren(tower, 'gynfwly'),
+  node => assoc(
+    'children',
+    node.children.map(child => assoc(
+      'fullWeight',
+      nodeWeight(tower, child.name),
+      child
+    )),
+    node
+  )
+)());
+
+console.log(pipe(
+  () => nodeWithFullChildren(tower, 'jjjks'),
+  node => assoc(
+    'children',
+    node.children.map(child => assoc(
+      'fullWeight',
+      nodeWeight(tower, child.name),
+      child
+    )),
+    node
+  )
+)());
+
+console.log(pipe(
+  () => nodeWithFullChildren(tower, 'gtervu'),
+  node => assoc(
+    'children',
+    node.children.map(child => assoc(
+      'fullWeight',
+      nodeWeight(tower, child.name),
+      child
+    )),
+    node
+  )
+)());
+
